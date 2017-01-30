2251c70dc58a1d33e19827c6a8d11a15c78532bb
guiand <guillaume.androz@icentia.com>
2017-01-30 16:22:46 -0500

    Prettify code

--- app/api_1_0/accounts.py
+++ app/api_1_0/accounts.py
@@ -2,10 +2,12 @@ from app.models import Account
 from . import api_blueprint as api
 from flask import jsonify
 
+
 @api.route('/accounts/')
 def get_accounts():
     accounts = Account.query.all()
-    return jsonify({ 'accounts': [account.to_json() for account in accounts] })
+    return jsonify({'accounts': [account.to_json() for account in accounts]})
+
 
 @api.route('/accounts/<int:id>')
 def get_account(id):
